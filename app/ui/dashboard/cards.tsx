4f37b2c27f2b213b69cca14286d57b074029b943
wellyton-xs <wellyton.offcer@gmail.com>
2024-05-18 11:49:29 -0300

    fix 'cannot find' on ui/dashboard/cards.tsx

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -5,6 +5,7 @@ import {
   InboxIcon,
 } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
+import { fetchCardData } from '@/app/lib/data';
 
 const iconMap = {
   collected: BanknotesIcon,
@@ -14,6 +15,12 @@ const iconMap = {
 };
 
 export default async function CardWrapper() {
+  const numberOfCustomers = await (await fetchCardData()).numberOfCustomers;
+  const totalPaidInvoices = await (await fetchCardData()).totalPaidInvoices;
+  const totalPendingInvoices = await (
+    await fetchCardData()
+  ).totalPendingInvoices;
+  const numberOfInvoices = await (await fetchCardData()).numberOfInvoices;
   return (
     <>
       {/* NOTE: comment in this code when you get to this point in the course */}
